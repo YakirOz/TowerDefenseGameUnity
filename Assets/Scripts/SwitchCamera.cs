ed677050ec87ce45cd81219fc8db8142e51269ba
Sagiv Michael <sagiv07@gmail.com>
2019-09-23 16:21:59 +0300

    adding Camera Switching botton.

---
+++ Assets/Scripts/SwitchCamera.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class SwitchCamera : MonoBehaviour
+{
+    public Camera[] cameras;
+    public AudioListener[] camerasListener;
+
+    public void switchCameras()
+    {
+        int cameraCounter = PlayerPrefs.GetInt("CameraPosition");
+        cameraCounter++;
+        if(cameraCounter > 1)
+        {
+            cameraCounter = 0;
+        }
+        PlayerPrefs.SetInt("CameraPosition", cameraCounter);
+
+        if (cameraCounter == 0 )
+        {
+            cameras[cameraCounter].enabled = true;
+            camerasListener[cameraCounter].enabled = true;
+            cameras[1 - cameraCounter].enabled = false;
+            camerasListener[1 - cameraCounter].enabled = false;
+        }
+
+        if (cameraCounter == 1)
+        {
+            cameras[cameraCounter].enabled = true;
+            camerasListener[cameraCounter].enabled = true;
+            cameras[1 - cameraCounter].enabled = false;
+            camerasListener[1 - cameraCounter].enabled = false;
+        }
+    }
+   
+}
